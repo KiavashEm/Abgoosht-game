252b798492649f1f400ccf94f496769bfe479a19
Kiavash Emrani <6305848@edu.vaniercollege.qc.ca>
2024-09-05 10:59:12 -0400

    this is my experiment

---
+++ MyOwnGame/zamin.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class zamin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class zamin extends World
+{
+
+    /**
+     * Constructor for objects of class zamin.
+     * 
+     */
+    public zamin()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
